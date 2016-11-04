b6492c2280c0a9bcbdc1e4bdf4578b7b3d2f2535
Juan Cornelio Guzman <jcornelio.guzman@gmail.com>
2016-11-04 22:46:26 +0800

    Complete handleAddTodo

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -9,5 +9,15 @@ var TodoApp = require('TodoApp');
 describe('TodoApp', () => {
   it('should exists', () => {
     expect(TodoApp).toExist();
-  })
+  });
+
+  it('should add todo to the state on handleAddTodo', () => {
+    var todoText = 'Add new task';
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+
+    todoApp.setState({todos: []});
+    todoApp.handleAddTodo(todoText);
+
+    expect(todoApp.state.todos[0].text).toBe(todoText);
+  });
 });
